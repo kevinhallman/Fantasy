ac9b0bc00f5a2bba32f9e69a48d2482e8213cbd1
contentedcow <hallmankevin@gmail.com>
2014-12-12 12:45:43 -0700

    version 3

--- bin/app.py
+++ bin/app.py
@@ -7,74 +7,116 @@ import re
 eventOrder=["50 Yard Freestyle","100 Yard Freestyle","200 Yard Freestyle",'500 Yard Freestyle','1000 Yard Freestyle',"1650 Yard Freestyle","100 Yard Butterfly","200 Yard Butterfly","100 Yard Backstroke","200 Yard Backstroke","100 Yard Breastroke","200 Yard Breastroke","200 Yard Individual Medley","400 Yard Individual Medley","200 Yard Medley Relay","400 Yard Medley Relay","200 Yard Freestyle Relay","400 Yard Freestyle Relay","800 Yard Freestyle Relay","1 mtr Diving","3 mtr Diving"]
 
 urls = (
-  '/', 'Swim',
+  '/', 'Home',
   '/swimulate','Swim',
   '/fantasy','Fantasy',
   '/conference','Conf',
   '/times','Times',
-  '/topDuals','Duals',
+  '/duals','Duals',
+  
 )
 
-app = web.application(urls, globals())
-
-render = web.template.render('templates/', base="layout")
+#web.config.debug = False
 
 meets.start(file='./bin/D3_15_12-8',gender='Men')
-database=meets.database
-topDuals = {}
+databaseMen = meets.database
 
-teamMeets = {}
-for team in database.teams:
-	teamMeets[team]=[]
-	for season in database.teams[team].meets:
-		for meet in database.teams[team].meets[season]:
-			meet = re.sub('\"','\\\\\"',meet)
-			teamMeets[team].append(meet)
+meets.start(file='./bin/D3_15_12-8',gender='Women')
+databaseWomen = meets.database
 
+topDuals = {'men':{},'women':{}}
+gender = 'men'
+division = 'D3'
 
-conferences = database.conferences
-confList = conferences.keys()
-confList.sort()
+app = web.application(urls, globals())
+render = web.template.render('templates/', base="layout")
+
+def getTeamMeets(database):
+	teamMeets = {}
+	for team in database.teams:
+		teamMeets[team]=[]
+		for season in database.teams[team].meets:
+			for meet in database.teams[team].meets[season]:
+				meet = re.sub('\"','\\\\\"',meet)
+				teamMeets[team].append(meet)
+	return teamMeets
+
+def getConfList(database):
+	conferences = database.conferences
+	confList = conferences.keys()
+	confList.sort()
+	return confList
+
+class Home():
+	def GET(self):
+		form = web.input(gender=None,division=None)
+		global gender,division
+		if form.gender and form.gender != gender:
+			gender = form.gender
+		if form.division and form.division != division:
+			division = form.division
+		return render.home(gender,division)
 
 class Swim(object):
 	def GET(self):
-		form = web.input(team1='',team2='')
-		if form.team1=='' or form.team2=='':
-			return render.swimulator(teamMeets,scores=None,fail=True,team1=None,team2=None,teamScores=None,finalScores=None)
+		if gender == 'women':
+			database = databaseWomen
+		else:
+			database = databaseMen		
+		teamMeets = getTeamMeets(database)
+		confList = getConfList(database)
+		
+		form = web.input(team1=None,team2=None,meet1=None,meet2=None)
+		keys = form.keys()
+		
+		teamsMeets = {}
+		createLineup = False
+		exit = False
+		for key in keys:
+			num = int(key[-1])
+			if not num in teamsMeets:
+				teamsMeets[num]=[None,None]
+			if "team" in key:
+				teamsMeets[num][0]=form[key]
+			elif "meet" in key:
+				if form[key]=='Create Lineup': #can't have two or more create lineups
+					if createLineup:
+						exit = True
+					else:
+						createLinup = True
+					teamsMeets[num][1]='Create Lineup'
+				else:
+					teamsMeets[num][1]=database.meets[form[key]]
+		
+		if exit: return render.swimulator(teamMeets,scores=None,teamScores=None,finalScores=None)	
+		
+		#use topDual if no meet?
+		remove=set()
+		optimizeTeam = None
+		for num in teamsMeets:
+			tm = teamsMeets[num]
+			if not tm[0] or not tm[1]:
+				remove.add(num)
+			elif tm[1] == 'Create Lineup':
+				optimizeTeam = tm[0]
+				remove.add(num)
+		for tm in remove:
+			del(teamsMeets[tm])
+		
+		if len(teamsMeets) < 1: return render.swimulator(teamMeets,scores=None,teamScores=None,finalScores=None)
 		
 		else:
-			team1 = database.teams[form.team1]
-			team2 = database.teams[form.team2]
-			if form.meet1 != "Create Lineup":	
-				meet1 = database.meets[form.meet1] #team1.topDual(meets.Season(2014),database = database)
+			newMeet = database.swimMeet(teamsMeets.values(),includeEvents=meets.requiredEvents,selectEvents=True,resetTimes=True)			
+			if optimizeTeam:
+				newMeet = database.lineup(optimizeTeam,newMeet)
+			if len(teamsMeets) > 2:
+				showNum = 20
 			else:
-				meet1 = form.meet1
-			if form.meet2 != "Create Lineup":
-				meet2 = database.meets[form.meet2] #team2.topDual(meets.Season(2014),database = database)
-			else:
-				meet2 = form.meet2
-			
-			newMeet = None
-			if meet1 == None or meet2 == None:
-				scores = None
-				fail = "Both teams haven't swum yet"
-			elif meet1 == "Create Lineup" and meet2 == "Create Lineup": 
-				scores = None
-				fail = "That's tricky"
-			elif meet1 == "Create Lineup":
-				newMeet = database.lineup(team2,team1,meet2,resetTimes=True)
-			elif meet2 == "Create Lineup":
-				newMeet = database.lineup(team1,team2,meet1,resetTimes=True)
-			else:
-				newMeet = database.swimMeet([[team1,meet1],[team2,meet2]],includeEvents=meets.requiredEvents,selectEvents=True,resetTimes=True)
-
-			if newMeet != None:
-				scores = newMeet.scoreString(showNum=6)
-				teamScores = newMeet.scoreReport(printout=False)
-				fail = False
-			else:
-				teamScores = None
-			return render.swimulator(teamMeets = teamMeets,scores = showMeet(scores),fail = fail,team1=team1,team2=team2,teamScores=showTeamScores(teamScores),finalScores=showScores(scores))
+				showNum = 6	
+			scores = newMeet.scoreString(showNum=showNum)
+			teamScores = newMeet.scoreReport(printout=False)
+				
+			return render.swimulator(teamMeets = teamMeets,scores = showMeet(scores),teamScores=showTeamScores(teamScores),finalScores=showScores(scores))
 
 class Fantasy(object):
 	def GET(self):
@@ -82,12 +124,20 @@ class Fantasy(object):
 		
 class Conf(object):
 	def GET(self):
+		if gender == 'women':
+			database = databaseWomen
+		else:
+			database = databaseMen
+			
+		teamMeets = getTeamMeets(database)
+		confList = getConfList(database)
+		
 		form = web.input(conference=None)
 		if form.conference:
 			if form.conference == 'Nationals':
 				confMeet = database.conference2(teams=database.teams.keys())
 				scores = confMeet.scoreString(25)
-				teamScores = confMeet.scoreReport(printout=False)
+				teamScores = confMeet.scoreReport(printout=False,repressSwim=True,repressTeam=True)
 			else:
 				conf = database.conferences[form.conference]
 				confMeet = database.conference2(teams=conf.teams)
@@ -98,29 +148,38 @@ class Conf(object):
 			teamScores = None
 		return render.conference(conferences=confList,scores = showMeet(scores),teamScores = showTeamScores(teamScores),finalScores=showScores(scores))
 		
+		
 class Times(object):
 	def GET(self):
 		return render.times()
 
 class Duals(object):
 	def GET(self):
+		if gender == 'women':
+			database = databaseWomen
+		else:
+			database = databaseMen
+			
+		teamMeets = getTeamMeets(database)
+		confList = getConfList(database)
+		
 		form = web.input(conference=None)
 		confName = form.conference
 		
 		if confName:
 			#cache
 			global topDuals
-			if not confName in topDuals:
+			if not confName in topDuals[gender]:
 				if confName in database.conferences:
 					teams = database.conferences[confName].teams
 				else:
 					teams = 'all'
-				topDuals[confName] = database.topDual(printout=False,teams = teams) #results,meets,wins,losses
+				topDuals[gender][confName] = database.topDual(printout=False,teams = teams) #results,meets,wins,losses
 			
-			wins = topDuals[confName][2]
-			losses = topDuals[confName][3]
+			wins = topDuals[gender][confName][2]
+			losses = topDuals[gender][confName][3]
 			teams = sorted(wins.items(),key=operator.itemgetter(1),reverse=True)
-			meets = topDuals[confName][1]
+			meets = topDuals[gender][confName][1]
 			for team in meets:
 				meets[team] = str(meets[team])
 		else:
@@ -130,6 +189,7 @@ class Duals(object):
 			meets = None
 		
 		return render.duals(wins = wins,losses = losses,teams = teams,meet = meets,conferences = confList)
+
 		
 #HTML generators
 
@@ -161,18 +221,25 @@ def showScores(scores):
 	html += '</table>'
 	return html
 	
-def showTeamScores(teamScores):
+def showTeamScores(teamScores,showType='swimmer'):
+	#type = swimmer,event, or year
 	if teamScores == None: return None
-	html = 	'<h2 align="center">	Score Report </h2>'
-	html += '<table>'
-	for team in teamScores:
-		html += '<tr> <th>'+team+'</th> </tr>'
-		for name in teamScores[team]:
-			if name[0] == 'Total': continue
-			html += '<tr>'
-			html += '<td>'+name[0]+'</td> <td>'+str(name[1])+'</td>'
-			html += '</tr>'
-	html += '</table>'
+	html = '<h2 align="center">	Score Report </h2>'
+	html += '<form>'
+	html += 'Show By: <select type="text" onchange="summaryType(this)">'
+	html += '<option>swimmer</option> <option>event</option> <option>year</option>'
+	html += '</select>'
+	for type in ['swimmer','event','year']:
+		if type==showType: html += '<table id=' + type + '>'
+		else: html += '<table class="hidden" id=' + type + '>'
+		for team in teamScores:
+			html += '<tr> <th>'+team+'</th> </tr>'
+			if not type in teamScores[team]: continue
+			for name in sorted(teamScores[team][type],key=teamScores[team][type].__getitem__,reverse=True):
+				html += '<tr>'
+				html += '<td>'+name+'</td> <td>'+str(teamScores[team][type][name])+'</td>'
+				html += '</tr>'
+		html += '</table>'
 	return html
 	
 if __name__ == "__main__":
