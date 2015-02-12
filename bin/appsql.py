035b68e0426859501c126427370163be73751698
contentedcow <hallmankevin@gmail.com>
2015-02-12 14:50:12 -0700

    prod

--- bin/appsql.py
+++ bin/appsql.py
@@ -22,7 +22,7 @@ urls = ('/', 'Home',
 	'/teamMeets', 'teamMeets'
 )
 
-prod = False
+prod = Trueg
 if prod:
 	urlparse.uses_netloc.append("postgres")
 	url = urlparse.urlparse(os.environ["DATABASE_URL"])
