853ec72b3dcac66fabd405a307821da7aebd8588
contentedcow <hallmankevin@gmail.com>
2018-01-31 21:51:06 -0800

    null json

--- bin/appsql.py
+++ bin/appsql.py
@@ -1072,7 +1072,7 @@ class TaperJSON():
 						taper, taperstd = teamseason.getTaperStats(weeks=week, yearsback=0, toptime=toptime)
 						#print team, week, taper
 						if taper < 0 or taper > 100 or isnan(taper):
-							taper = ''
+							taper = 'null'
 						if not team in tapers:
 							tapers[team] = {}
 						tapers[team][week] = taper
